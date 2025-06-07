2e46462bf2d4ae4ac941ce2bad16cc273d9a218d
Domene <domene@Domenes-Mac-mini.local>
2025-06-06 21:38:53 -0400

    clean up argparse class a bit

--- ArgumentParser.py
+++ ArgumentParser.py
@@ -5,19 +5,14 @@ class CLIArgumentParser:
     def parse():
         parser = argparse.ArgumentParser(description='CLI Toolkit to launch developer productivity.')
         sub_parser = parser.add_subparsers(dest='command', help='Available commands')
-
-        # Prompt parser
-        prompt_parser = sub_parser.add_parser('prompt', help='Natural language prompt to generate a CLI command for.')
-        prompt_parser.add_argument('prompt', help='The prompt to be evaluated for command generation.')
-
-        # Chat parser
-        chat_parser = sub_parser.add_parser('chat', help='A one-off chat with an LLM that you can prompt from your command line. Does not relate to any existing thread')
-        chat_parser.add_argument('chat', help='The prompt to be responded to.')
-        
+        # Create a mutually exclusive group
+        group = parser.add_mutually_exclusive_group(required=True)
+    
+        # Add `--prompt` or `-p` argument
+        group.add_argument('--prompt', '-p', type=str, help='Provide a prompt text', metavar='<text>')
+    
+        # Add `--chat` or `-c` argument
+        group.add_argument('--chat', '-c', type=str, help='Provide chat text', metavar='<text>')
+    
         args = parser.parse_args()
-        has_prompt = args.command == 'prompt'
-        has_chat = args.command == 'chat'
-        prompt = None if not has_prompt else args.prompt
-        chat = None if not has_chat else args.chat
-        return prompt, chat
-
+        return args.prompt, args.chat
