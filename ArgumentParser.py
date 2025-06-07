e861abf8ae9500eea276b6ca30964f5a68285329
Domene <domene@Domenes-Mac-mini.local>
2025-06-06 20:06:47 -0400

    Extract argparser to class + typo

---
+++ ArgumentParser.py
@@ -0,0 +1,23 @@
+import argparse
+
+class CLIArgumentParser:
+    @staticmethod
+    def parse():
+        parser = argparse.ArgumentParser(description='Generate CLI commands from natural language using an LLM.')
+        sub_parser = parser.add_subparsers(dest='command', help='Available commands')
+
+        # Prompt parser
+        prompt_parser = sub_parser.add_parser('prompt', help='Natural language prompt to generate a CLI command for.')
+        prompt_parser.add_argument('prompt', help='The prompt to be evaluated for command generation.')
+
+        # Chat parser
+        chat_parser = sub_parser.add_parser('chat', help='A one-off chat with an LLM that you can prompt from your command line. Does not relate to any existing thread')
+        chat_parser.add_argument('chat', help='The prompt to be responded to.')
+        
+        args = parser.parse_args()
+        has_prompt = args.command == 'prompt'
+        has_chat = args.command == 'chat'
+        prompt = None if not has_prompt else args.prompt
+        chat = None if not has_chat else args.chat
+        return prompt, chat
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import subprocess
 from providers.openai import OpenAIModelProvider
-
+from ArgumentParser import CLIArgumentParser
 
 def get_env_vars():
     return dict(os.environ)
@@ -20,19 +20,8 @@ def get_installed_clis():
     return sorted(clis)
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate CLI commands from natural language using an LLM.")
-    sub_parser = parser.add_subparsers(dest='command', help='Available commands')
-    prompt_parser = sub_parser.add_parser('prompt', help='Natural language prompt to generate a CLI command for.')
-    prompt_parser.add_argument('prompt', help='The prompt to be evaluated for command generation.')
-    chat_parser = sub_parser.add_parser('chat', help='A one-off chat with an LLM that you can prompt from your command line. Does not relate to any existing thread')
-    chat_parser.add_argument('chat', help='The prompt to be responded to.')
+    prompt, chat = CLIArgumentParser.parse()
 
-    args = parser.parse_args()
-    hasPrompt = args.command == 'prompt'
-    hasChat = args.command == 'chat'
-    prompt = None if not hasPrompt else args.prompt
-    chat = None if not hasChat else args.chat
-    
     api_key = os.environ.get("OPENAI_API_KEY")
     if not api_key:
         print("Error: Please set the OPENAI_API_KEY environment variable.")
@@ -62,5 +51,6 @@ def main():
                     print(f"\nError running command: {e}")
     if chat:
         print(provider.generate_chat(chat))
+
 if __name__ == "__main__":
     main()
