77ddd126efc364a7605286e70f6eb85b900a3385
Domene <domene@Domenes-Mac-mini.local>
2025-06-09 17:25:27 -0400

    fix to test

--- cli_platform/tests/argument_parser_test.py
+++ cli_platform/tests/argument_parser_test.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ArgumentParser import *
+from argument_parser import *
 
 LONG_FORM_PROMPT = '--prompt'
 LONG_FORM_CHAT = '--chat'
